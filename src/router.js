4bebd5d9d120087d97a2bf424f220c08ab32bc86
CyberGhost04 <sidhantbahuguna@gmail.com>
2024-08-11 16:39:20 +0530

    download button functional

--- src/router.js
+++ src/router.js
@@ -22,8 +22,11 @@ module.exports = (server) => {
   // ------------------------------------------------ //
       // ************ VIDEO ROUTES ************* //
   // ------------------------------------------------ //
-  server.route("get", "/api/videos-temp", Video.getVideos);
+  server.route("get", "/api/videos", Video.getVideos);
 
   // Upload a video file
   server.route("post", "/api/upload-video", Video.uploadVideo);
+
+  // Return a video asset to the client
+  server.route("get", "/get-video-asset", Video.getVideoAsset);
 };
